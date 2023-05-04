7ba97922a73e14860f97a517961c317c4880a34c
Mansi S <mansisaini26@gmail.com>
2023-05-03 20:12:54 -0700

    update scenes and keys

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ let config = {
     type: Phaser.CANVAS, // init game canvas
     width: 640, // init width
     height: 480, // init height
-    scene: [ Menu, GameControls, Play] // init menu and play scenes
+    scene: [ Menu, Play] // init menu and play scenes
 }
 
 let game = new Phaser.Game(config); // init new phaser game
@@ -14,7 +14,7 @@ let borderUISize = game.config.height / 15; // set UI height
 let borderPadding = borderUISize / 3; // set padding around game frame
 
 // reserve keyboard vars
-let keyF, keyR, keyLEFT, keyRIGHT; 
+let keyF, keyR, keyLEFT, keyRIGHT, keySPACE, keyDOWN; 
 
 // reserve mouse var
 let mouse, mouseMove;
