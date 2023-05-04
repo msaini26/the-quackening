675298e2c1a06089f336f5b9c7b75c016a3ec752
Mansi S <mansisaini26@gmail.com>
2023-05-03 20:25:09 -0700

    init collision

--- src/prefabs/Platform.js
+++ src/prefabs/Platform.js
@@ -1,5 +1,10 @@
 class Platform extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, width, height) {
         super(scene, x, y, width, height);
+        this.activated = true;
+    }
+
+    collision(rocket) {     // standard collision behavior - leave empty if stays static
+    
     }
 }
