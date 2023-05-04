aeab4d3ad4dffb00d085d002a7a757c5e977d63d
Mansi S <mansisaini26@gmail.com>
2023-05-03 21:26:21 -0700

    rocket jump

--- src/prefabs/Platform.js
+++ src/prefabs/Platform.js
@@ -1,10 +1,24 @@
 class Platform extends Phaser.GameObjects.Sprite{
-    constructor(scene, x, y, width, height) {
-        super(scene, x, y, width, height);
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
         this.activated = true;
+
+        scene.add.existing(this);
     }
 
     collision(rocket) {     // standard collision behavior - leave empty if stays static
-    
+
+        console.log("from Platform.js: from collision(): colliding");
+        
+        if (((rocket.x - rocket.width/2) < (this.x - this.width/2)) || ((rocket.x + rocket.width/2) < (this.x + this.width/2))) {
+
+            rocket.peaked = true;
+            rocket.jump()
+
+        } else {
+            rocket.platformReset();
+            rocket.currPlat = this;
+        }
+        
     }
 }
