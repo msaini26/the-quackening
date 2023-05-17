ce0798f8dc3a7f8e4bd15afef5b30cf8cbd17a97
Mansi S <mansisaini26@gmail.com>
2023-05-16 22:11:17 -0700

    init new project

---
+++ src/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    scene: [Play], //add menu later
+}
+
+let game = new Phaser.Game(config);
---
+++ src/scenes/Play.js
@@ -0,0 +1,20 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+
+    // preload assets
+    preload() {
+
+    }
+
+    // create objects and instances in phaser canvas
+    create() {
+
+    }
+
+    // updates every frame
+    update() {
+        
+    }
+}
